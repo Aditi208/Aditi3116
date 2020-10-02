61f6a6a24ce7d74d9e0c330647bfc26c1cf0d665
jOh <aditisingh3116@gmail.com>
2020-10-01 19:48:44 -0700

    Initial Commit

---
+++ p1.py
@@ -0,0 +1,11 @@
+name = input("Pls enter your Name: ")
+branch = input("Tell me your Branch: ")
+gender = input("What is your Gender: ")
+collegeName = input("What's your College Name: ")
+age = int(input(" Pls enter your Age: "))
+print("Name:",name)
+print("Branch:",branch)
+print("Gender:",gender)
+print("College Name:",collegeName)
+print("Age:",age)
+
